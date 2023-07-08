1c494d68b2e6b03f847041b1eb04d7fbe34a1b8c
unknown <neilmagic25@gmail.com>
2023-07-07 21:10:34 -0400

    creating and restarting this assignment again

---
+++ cd_project/localsettings.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@*@49^0w*l1x2xo8izwt@=tonuww+p^^4lquc7(&$lxbtit9l@'
+
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cd_database',
+        'HOST': 'localhost',
+        'USER' : 'root',
+        'PASSWORD' : 'Sanders20'
+    }
+}
